413e40531a069cde763518acbc96c35731371af0
aviwer <prakhar24012000@gmail.com>
2022-06-08 02:08:16 +0530

    final commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,41 +1,25 @@
 const Salestransaction = require('../models/salesTransaction')
 const Paymenttransaction = require('../models/paymentTransaction')
-const jwt = require('jsonwebtoken')
-exports.Login = async (req,res)=>{
+exports.api = async(req,res)=>{
     try {
-        const token = jwt.sign({username:req.body.username, password:req.body.password},process.env.JWT_SECRET);
-        const obj = {
-            access_token: token,
-            token_type:"bearer",
-            message:"token generated successfully"
+        if(req.query.grant_type == "Sales"){
+            const salesTransaction = new Salestransaction(req.body)
+            await salesTransaction.save()
+            res.status(201).json({
+                transaction:{
+                    salesTransaction
+                }
+            })
         }
-        res.send(JSON.stringify(obj));
-    } catch (error) {
-        res.status(400).send();
-    }
-}
-exports.salesTransactionEntry = async(req,res)=>{
-    const salesTransaction = new Salestransaction(req.body)
-    try {
-        await salesTransaction.save()
-        res.status(201).json({
-            transaction:{
-                salesTransaction
-            }
-        })
-    } catch (error) {
-        res.status(400).send()
-    }
-}
-exports.paymentTransactionEntry = async(req,res)=>{
-    const paymentTransaction = new Paymenttransaction(req.body)
-    try {
-        await paymentTransaction.save()
-        res.status(201).json({
-            transaction:{
-                paymentTransaction
-            }
-        })
+        else if(req.query.grant_type == "Payment"){
+            const paymentTransaction = new Paymenttransaction(req.body)
+            await paymentTransaction.save()
+            res.status(201).json({
+                transaction:{
+                    paymentTransaction
+                }
+            })
+        }
     } catch (error) {
         res.status(400).send()
     }
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const Controller = require("./../controllers/controller");
-const auth = require('../middleware/auth')
 const router = express.Router();
-router.post("/login/dataapi",Controller.Login);
-router.post("/login/dataapi/sales",auth,Controller.salesTransactionEntry);
-router.post("/login/dataapi/payment",auth,Controller.paymentTransactionEntry);
+router.post("/login/dataapi",Controller.api);
 module.exports = router
