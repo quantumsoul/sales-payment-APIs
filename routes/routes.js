5abe10eb797c0bd51cd9d7a364c118cdf49bc6c3
aviwer <prakhar24012000@gmail.com>
2022-06-07 17:51:35 +0530

    test deploy

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Controller = require("./../controllers/Controller");
 const auth = require('../middleware/auth')
 const router = express.Router();
-router.post("/login/dataapi?grant_type=token",Controller.Login);
-router.post("/login/dataapi?grant_type=Sales",auth,Controller.salesTransactionEntry);
-router.post("/login/dataapi?grant_type=Payment",auth,Controller.paymentTransactionEntry);
+router.post("/login",Controller.Login);
+router.post("/login/sales",auth,Controller.salesTransactionEntry);
+router.post("/login/payment",auth,Controller.paymentTransactionEntry);
 module.exports = router
