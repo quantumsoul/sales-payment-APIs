bd65bbbb3825d415711ef956d0bdd917ae046ba4
aviwer <prakhar24012000@gmail.com>
2022-06-07 19:09:28 +0530

    y

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Controller = require("./../controllers/controller");
 const auth = require('../middleware/auth')
 const router = express.Router();
-router.post("/login",Controller.Login);
+router.post("/login/dataapi",Controller.Login);
 router.post("/login/sales",auth,Controller.salesTransactionEntry);
 router.post("/login/payment",auth,Controller.paymentTransactionEntry);
 module.exports = router
