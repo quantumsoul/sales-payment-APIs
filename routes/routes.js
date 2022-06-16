bc133f5ec81833933b959308ed86cb9c6357bde5
aviwer <prakhar24012000@gmail.com>
2022-06-16 19:30:43 +0530

    added auth

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const auth = require("../middleware/auth");
 const Controller = require("./../controllers/controller");
 const router = express.Router();
-router.post("/login/dataapi",Controller.postapi);
-router.get("/login/dataapi", Controller.getapi);
+router.post("/login/dataapi",auth,Controller.postapi);
+router.get("/login/dataapi",auth,Controller.getapi);
 router.get("/login",Controller.login);
 router.get("/loginInfo",auth,Controller.loginInfo);
 module.exports = router
