edc601c965f14d90525524a7fdb1c5e547a5b420
aviwer <prakhar24012000@gmail.com>
2022-06-17 11:46:32 +0530

    date filter and get req added

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,7 +3,7 @@ const Paymenttransaction = require('../models/paymentTransaction')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 exports.login = async(req,res)=>{
-    const prevuser = await User.findOne({username:req.body.username,password:req.body.password})
+    const prevuser = await User.findOne({username:req.headers.username,password:req.headers.password})
     if(prevuser!=undefined){
         const token = await prevuser.generateAuthToken()
         res.status(200).send({
@@ -12,7 +12,7 @@ exports.login = async(req,res)=>{
             "message":"token generated successfully",
         })
     } else {
-        const user = new User(req.body)
+        const user = new User({username:req.headers.username,password:req.headers.password})
         try {
             await user.save()
             const token = await user.generateAuthToken()
@@ -58,11 +58,14 @@ exports.postapi = async(req,res)=>{
 exports.getapi = async(req,res)=>{
     try {
         if(req.query.grant_type == "Sales"){
-
+            var b = req.headers.FromDate.split('-')
+            var fromDate = b[2] + '-' + b[1] + '-' + b[0]
+            var c = req.headers.ToDate.split('-')
+            var toDate = c[2] + '-' + c[1] + '-' + c[0]
             const Transactions = await Salestransaction.find({
                 BILL_DT: {
-                    $gte: new Date(new Date(req.headers.FromDate).setHours(00, 00, 00)),
-                    $lt: new Date(new Date(req.headers.ToDate).setHours(23, 59, 59))
+                    $gte: new Date(new Date(fromDate).setHours(00, 00, 00)),
+                    $lt: new Date(new Date(toDate).setHours(23, 59, 59))
                 }
             }).sort({ BILL_DT: 'asc'})
             
@@ -71,10 +74,14 @@ exports.getapi = async(req,res)=>{
             })
         }
         else if(req.query.grant_type == "Payment"){
+            var b = req.headers.FromDate.split('-')
+            var fromDate = b[2] + '-' + b[1] + '-' + b[0]
+            var c = req.headers.ToDate.split('-')
+            var toDate = c[2] + '-' + c[1] + '-' + c[0]
             const Transactions = await Paymenttransaction.find({
                 DOC_DT: {
-                    $gte: new Date(new Date(req.headers.FromDate).setHours(00, 00, 00)),
-                    $lt: new Date(new Date(req.headers.ToDate).setHours(23, 59, 59))
+                    $gte: new Date(new Date(fromDate).setHours(00, 00, 00)),
+                    $lt: new Date(new Date(toDate).setHours(23, 59, 59))
                 }
             }).sort({ DOC_DT: 'asc'})
             res.status(201).json({
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,6 @@ const Controller = require("./../controllers/controller");
 const router = express.Router();
 router.post("/login/dataapi",auth,Controller.postapi);
 router.get("/login/dataapi",auth,Controller.getapi);
-router.post("/login",Controller.login);
+router.get("/login",Controller.login);
 router.get("/loginInfo",auth,Controller.loginInfo);
 module.exports = router
